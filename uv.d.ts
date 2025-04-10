15ee807ed4f489487e7a02ecfdc05f59a58875cb
ZeroneV1 <164795172+ZeroneV1@users.noreply.github.com>
2025-04-10 08:42:56 -0500

    Update uv.d.ts

--- uv.d.ts
+++ uv.d.ts
@@ -5,7 +5,7 @@ export type UltravioletCtor = typeof Ultraviolet;
 export type UVClientCtor = typeof UVClient;
 
 /**
- * The proxy part of the URL.
+ * The part of the URL.
  */
 type Coded = string;
 
